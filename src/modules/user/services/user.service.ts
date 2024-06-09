8d345702aec924e5384572c790741191b43529da
Nakk-i <nastyabond90@gmail.com>
2024-06-09 13:31:53 +0300

    Add user + articles

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,34 +1,94 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 import { IUserData } from '../../auth/interfaces/user-data.interface';
 import { LoggerService } from '../../logger/logger.service';
+import { FollowRepository } from '../../repository/services/follow.repository';
 import { UserRepository } from '../../repository/services/user.repository';
 import { UpdateUserReqDto } from '../dto/req/update-user.req.dto';
+import { UserResDto } from '../dto/res/user.res.dto';
+import { UserMapper } from './user.mapper';
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly logger: LoggerService,
     private readonly userRepository: UserRepository,
+    private readonly followRepository: FollowRepository,
   ) {}
 
-  public async findAll(): Promise<any> {
-    return `This action returns all user`;
+  public async getMe(userData: IUserData): Promise<UserResDto> {
+    const user = await this.userRepository.findOneBy({ id: userData.userId });
+    return UserMapper.toResponseDTO(user);
   }
 
-  public async findOne(id: string): Promise<any> {
-    return `This action returns a #${id} user`;
-  }
-
-  public async update(
-    id: string,
+  public async updateMe(
+    userData: IUserData,
     updateUserDto: UpdateUserReqDto,
-  ): Promise<any> {
-    return `This action updates a #${id} user`;
+  ): Promise<UserResDto> {
+    const user = await this.userRepository.findOneBy({ id: userData.userId });
+    const updateUser = await this.userRepository.save({
+      ...user,
+      ...updateUserDto,
+    });
+    return UserMapper.toResponseDTO(updateUser);
   }
 
-  public async remove(id: string): Promise<any> {
-    return `This action removes a #${id} user`;
+  public async removeMe(userData: IUserData): Promise<void> {
+    const user = await this.userRepository.findOneBy({ id: userData.userId });
+    await this.userRepository.remove(user);
+  }
+
+  public async getById(id: string): Promise<UserResDto> {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return UserMapper.toResponseDTO(user);
+  }
+
+  public async follow(userData: IUserData, userId: string): Promise<void> {
+    if (userData.userId === userId) {
+      throw new ConflictException('You cannot follow yourself');
+    }
+    const user = await this.userRepository.findOneBy({ id: userId });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const follow = await this.followRepository.findOneBy({
+      follower_id: userData.userId,
+      following_id: userId,
+    });
+    if (follow) {
+      throw new ConflictException('You are already following this user');
+    }
+    await this.followRepository.save(
+      this.followRepository.create({
+        follower_id: userData.userId,
+        following_id: userId,
+      }),
+    );
+  }
+
+  public async unfollow(userData: IUserData, userId: string): Promise<void> {
+    if (userData.userId === userId) {
+      throw new ConflictException('You cannot follow yourself');
+    }
+    const user = await this.userRepository.findOneBy({ id: userId });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const follow = await this.followRepository.findOneBy({
+      follower_id: userData.userId,
+      following_id: userId,
+    });
+    if (!follow) {
+      throw new ConflictException('You cant unfollow this user');
+    }
+    await this.followRepository.remove(follow);
   }
 
   public async isEmailUniqueOrThrow(email: string): Promise<void> {
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,15 +5,20 @@ import {
   Get,
   Param,
   ParseUUIDPipe,
-  Patch,
+  Post,
+  Put,
 } from '@nestjs/common';
 import {
+  ApiBearerAuth,
   ApiForbiddenResponse,
   ApiNotFoundResponse,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { SkipAuth } from '../auth/decorators/skip-auth.decorator';
+import { IUserData } from '../auth/interfaces/user-data.interface';
 import { UpdateUserReqDto } from './dto/req/update-user.req.dto';
 import { UserResDto } from './dto/res/user.res.dto';
 import { UserService } from './services/user.service';
@@ -23,32 +28,60 @@ import { UserService } from './services/user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiBearerAuth()
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @Get('me')
+  public async getMe(@CurrentUser() userData: IUserData): Promise<UserResDto> {
+    return await this.userService.getMe(userData);
+  }
+
+  @ApiBearerAuth()
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @Put('me')
+  public async updateMe(
+    @CurrentUser() userData: IUserData,
+    @Body() updateUserDto: UpdateUserReqDto,
+  ): Promise<UserResDto> {
+    return await this.userService.updateMe(userData, updateUserDto);
+  }
+
+  @ApiBearerAuth()
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @Delete('me')
+  public async removeMe(@CurrentUser() userData: IUserData): Promise<void> {
+    return await this.userService.removeMe(userData);
+  }
+  @SkipAuth()
   @ApiForbiddenResponse({ description: 'Forbidden' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   @ApiNotFoundResponse({ description: 'Not Found' })
   @Get(':id')
-  public async findOne(
+  public async getById(
     @Param('id', ParseUUIDPipe) id: string,
   ): Promise<UserResDto> {
-    return await this.userService.findOne(id);
+    return await this.userService.getById(id);
+  }
+  @ApiBearerAuth()
+  @Post(':userId/follow')
+  public async follow(
+    @CurrentUser() userData: IUserData,
+    @Param('userId', ParseUUIDPipe) userId: string,
+  ): Promise<void> {
+    await this.userService.follow(userData, userId);
   }
 
-  @ApiForbiddenResponse({ description: 'Forbidden' })
-  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
-  @ApiNotFoundResponse({ description: 'Not Found' })
-  @Patch(':id')
-  public async update(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() updateUserDto: UpdateUserReqDto,
-  ): Promise<any> {
-    return await this.userService.update(id, updateUserDto);
-  }
-
-  @ApiForbiddenResponse({ description: 'Forbidden' })
-  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
-  @ApiNotFoundResponse({ description: 'Not Found' })
-  @Delete(':id')
-  public async remove(@Param('id', ParseUUIDPipe) id: string): Promise<any> {
-    return await this.userService.remove(id);
+  @ApiBearerAuth()
+  @Delete(':userId/follow')
+  public async unfollow(
+    @CurrentUser() userData: IUserData,
+    @Param('userId', ParseUUIDPipe) userId: string,
+  ): Promise<void> {
+    await this.userService.unfollow(userData, userId);
   }
 }
