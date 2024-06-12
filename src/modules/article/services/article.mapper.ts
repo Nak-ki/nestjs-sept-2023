7077d41590a67702b7377cec379c4660fe42affc
Nakk-i <nastyabond90@gmail.com>
2024-06-12 14:08:20 +0300

    Add articles

--- src/modules/article/services/article.mapper.ts
+++ src/modules/article/services/article.mapper.ts
@@ -1,4 +1,5 @@
 import { ArticleEntity } from '../../../database/entities/article.entity';
+import { UserMapper } from '../../user/services/user.mapper';
 import { ArticleResDto } from '../dto/res/article.res.dto';
 
 export class ArticleMapper {
@@ -11,6 +12,7 @@ export class ArticleMapper {
       created: entity.created,
       updated: entity.updated,
       tags: entity.tags.map((tag) => tag.name),
+      user: entity.user ? UserMapper.toResponseDTO(entity.user) : null,
     };
   }
 }
